62306441777dae223de105f10803f8dfa380a272
vugtboy <jurianvanvugt@hotmail.com>
2025-12-16 16:31:58 +0100

    Login page gemaakt
    je kan wachtwoord en email geven dan stuurt hij deze naar de backend

--- frontend/frontend-app/src/Pages/Login.tsx
+++ frontend/frontend-app/src/Pages/Login.tsx
@@ -1,11 +1,67 @@
-import '../App.css'
+import { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch("http://localhost:3000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Login mislukt");
+      }
+
+      const data = await response.json();
+      console.log("Ingelogd:", data);
+      return;
+    } catch (err) {
+      setError("Verkeerde email of wachtwoord")
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <>
+    <form onSubmit={handleSubmit}>
       <h1>Login</h1>
-    </>
-  )
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+
+      <input
+        type="password"
+        placeholder="Wachtwoord"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+
+      <button type="submit">Inloggen</button>
+
+      {error ? <p>{error}</p> : null}
+    </form>
+  );
 }
 
-export default Login
+export default Login;
